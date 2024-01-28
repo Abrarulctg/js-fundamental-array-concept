ffbcf9a59150f9f0b422ff1c09e56aaf4555569b
Abrarulctg <jubairctg99@gmail.com>
2024-01-28 16:27:04 +0600

    learned js index, get, set by index value

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const numbers = [12, 56, 12, 36, 25];
+const numbers = [12, 56, 12, 36, 25, 56, 20, 25, 56, 20, 25, 56, 20, 25, 56, 20];
+
+console.log(numbers);
+//get Element value by index
+console.log(numbers[4]);
+
+const forth = numbers[3];
+console.log(forth);
+
+// set or update element value by index
+let abc = 99;
+abc = 102;
+
+numbers[1] = 1122;
+console.log(numbers)
+
