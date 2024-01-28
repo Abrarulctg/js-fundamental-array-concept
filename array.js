05d56a62c4f5d5c05272813f174ed004d6cc308c
Abrarulctg <jubairctg99@gmail.com>
2024-01-28 16:06:14 +0600

    initialized module

---
+++ array.js
@@ -0,0 +1,8 @@
+// const num1 = 12;
+// const num2 = 15;
+// const num3 = 16;
+// const num4 = 18;
+// const num5 = 18;
+
+const numbers = [12, 56, 12, 36, 25];
+console.log(numbers);
---
+++ length.js
@@ -0,0 +1,3 @@
+const numbers = [12, 56, 12, 36, 25, 5, 6, 9, 1, 33, 82, 41];
+
+console.log(numbers.length)
