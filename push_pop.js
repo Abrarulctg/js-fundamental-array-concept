e445c74102a59bf6eaa533f9a1c4789f451ac4ef
Abrarulctg <jubairctg99@gmail.com>
2024-01-29 00:47:18 +0600

    added push.js

---
+++ push_pop.js
@@ -0,0 +1,11 @@
+const ages = [];
+const numbers = [55, 23, 6, 15, 66];
+
+console.log(numbers);
+
+numbers.push(56);
+
+console.log(numbers);
+
+numbers.push(31, 55, 69, 2201);
+console.log(numbers);
