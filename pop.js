e6e78318b3c1c6b68d0158645420e69ba7ddd4a9
Abrarulctg <jubairctg99@gmail.com>
2024-01-29 00:55:41 +0600

    Practiced shift, unshift.js

---
+++ pop.js
@@ -0,0 +1,11 @@
+const friends = ['abrar', 'zihad', 'fayez', 'morshed', 'tarek', 'nafis'];
+// friends.push('lubaba');
+console.log(friends)
+
+const out2 = friends.pop()
+console.log(friends)
+const out1 = friends.pop()
+console.log(friends)
+console.log(out1, out2)
+
+
---
+++ shift_unshift.js
@@ -0,0 +1,7 @@
+const friends = ['abrar', 'zihad', 'fayez', 'morshed', 'tarek', 'nafis'];
+
+console.log(friends)
+friends.shift();
+console.log(friends)
+friends.unshift('jubair');
+console.log(friends)
